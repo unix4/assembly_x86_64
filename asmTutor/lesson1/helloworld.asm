df2004a536c1b7fea82b9d5fdc10f725657f32ba
unix4 <69163299+unix4@users.noreply.github.com>
2022-03-07 08:47:13 +0100

    Create helloworld.asm

---
+++ asmTutor/lesson1/helloworld.asm
@@ -0,0 +1,11 @@
+SECTION .data
+msg     db      'Hello World!', 0Ah
+SECTION .text
+global _start
+
+_start:
+        mov     edx, 13
+        mov     ecx, msg
+        mov     ebx, 1
+        mov     eax, 4
+        int     80h
